4eed6eda3b0b1bc61e4469797659788734d92d0e
BORRUSTA <125168234+BORRUSTA@users.noreply.github.com>
2023-02-12 09:34:10 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,7 @@
+function popupku() {
+  var body = document.querySelector("body");      
+  var pWm = document.createElement("a");
+  pWm.setAttribute("href", "https://youtube.com/dekatutorial");
+  pWm.innerHTML = "Youtube : Deka Tutorial";
+  body.appendChild(pWm);
+}
